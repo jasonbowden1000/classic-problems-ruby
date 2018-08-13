47214898000bc4dbd5bca1a8befff6c0dae79025
Jason Bowden <jason.bowden1000@gmail.com>
2018-08-12 19:32:39 -0500

    Made math spec less verbose

--- spec/problems/math_spec.rb
+++ spec/problems/math_spec.rb
@@ -6,21 +6,15 @@ describe 'Math' do
 
   context "when reversing digits" do
     it 'should return a number in reverse order' do
-      num = 4242
-      reversed_num = 2424
-      expect(math.reverse_digits(num)).to eq(reversed_num)
+      expect(math.reverse_digits(4242)).to eq(2424)
     end
 
     it 'should preserve the sign of the reversed digits' do
-      num = -314
-      reversed_num = -413
-      expect(math.reverse_digits(num)).to eq(reversed_num)
+      expect(math.reverse_digits(-314)).to eq(-413)
     end
 
     it 'should not return octals for numbers ending with a zero' do
-      num = 1230
-      reversed_num = 321
-      expect(math.reverse_digits(num)).to eq(reversed_num)
+      expect(math.reverse_digits(1230)).to eq(321)
     end
   end
 end
