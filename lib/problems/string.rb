513164a7402f1345b3f0ff4dd34e2c77fc86a493
Jason Bowden <jason.bowden1000@gmail.com>
2018-08-20 00:18:10 -0500

    Roman numerals

--- lib/problems/string.rb
+++ lib/problems/string.rb
@@ -1,8 +1,34 @@
 module Problems
   class String
+    ROMAN_HASH = {
+      "I" => 1, "IV" => 4, "V" => 5, "IX" => 9, "X" => 10, "XL" => 40,
+      "L" => 50, "XC" => 90, "C" => 100, "CD" => 400, "D" => 500, "M" => 1000
+    }
+    def arabicToRoman(n)
+      ROMAN_HASH.reverse_each.to_h.inject('') do |acc, (roman, arabic)|
+        while n >= arabic
+          acc = acc + roman
+          n -= arabic
+        end
+        acc
+      end
+    end
+
     def palindrome?(str)
       letters = str.downcase.scan(/\w/)
       letters == letters.reverse
+    end
+
+    def romanToArabic(str)
+      str = str.split("").map { |n| ROMAN_HASH[n] }.reverse
+      str.each_cons(2).inject(str[0]) do |acc, (prev, curr)|
+        curr >= prev ? acc + curr : acc - curr
+      end
+    end
+
+    def roman?(str)
+      re = /^M{0,3}(CM|CD|D?C{0,3})(XC|XL|L?X{0,3})(IX|IV|V?I{0,3})$/
+      !!(re =~ str)
     end
   end
 end
--- spec/problems/string_spec.rb
+++ spec/problems/string_spec.rb
@@ -1,26 +1,89 @@
 require 'spec_helper'
 require 'problems/string'
 
-describe 'palindrome?' do
+describe 'String processing' do
   let (:string) { Problems::String.new }
 
-  it 'should accept a basic palindromic word' do
-    expect(string.palindrome?('')).to be true
-    expect(string.palindrome?('a')).to be true
-    expect(string.palindrome?('rotator')).to be true
-    expect(string.palindrome?('redder')).to be true
-  end
-  it 'should accept a capitalized palindromic word' do
-    expect(string.palindrome?('Madam')).to be true
+  describe 'palindrome?' do
+    it 'should accept a basic palindromic word' do
+      expect(string.palindrome?('')).to be true
+      expect(string.palindrome?('a')).to be true
+      expect(string.palindrome?('rotator')).to be true
+      expect(string.palindrome?('redder')).to be true
+    end
+    it 'should accept a capitalized palindromic word' do
+      expect(string.palindrome?('Madam')).to be true
+    end
+
+    it 'should accept a palindromic sentence with non-alphabetic characters' do
+      expect(string.palindrome?('A car, a man, a maraca')).to be true
+    end
+
+    it 'should reject non-palindromic words' do
+      expect(string.palindrome?('jason')).to be false
+      expect(string.palindrome?('dude')).to be false
+      expect(string.palindrome?('axyza')).to be false
+    end
   end
 
-  it 'should accept a palindromic sentence with non-alphabetic characters' do
-    expect(string.palindrome?('A car, a man, a maraca')).to be true
+  describe 'romanToArabic' do
+    it 'should return its arabic representation' do
+      expect(string.romanToArabic('MDCCCLXVI')).to equal(1866);
+      expect(string.romanToArabic('XIV')).to equal(14);
+      expect(string.romanToArabic('LXXXIX')).to equal(89);
+      expect(string.romanToArabic('XCI')).to equal(91);
+      expect(string.romanToArabic('DCCCXC')).to equal(890);
+      expect(string.romanToArabic('MCMLXXXIX')).to equal(1989);
+    end
   end
 
-  it 'should reject non-palindromic words' do
-    expect(string.palindrome?('jason')).to be false
-    expect(string.palindrome?('dude')).to be false
-    expect(string.palindrome?('axyza')).to be false
+  describe 'roman?' do
+    it 'should not accept more than three consecutive numerals' do
+      expect(string.roman?('XXXX')).to be false
+    end
+    it 'should reject any sequential repetition of V, L, or D' do
+      expect(string.roman?('LL')).to be false
+      expect(string.roman?('DDC')).to be false
+      expect(string.roman?('XVV')).to be false
+    end
+    it 'should allow valid exceptions of order' do
+      expect(string.roman?('IV')).to be true
+      expect(string.roman?('IX')).to be true
+      expect(string.roman?('XC')).to be true
+      expect(string.roman?('XL')).to be true
+      expect(string.roman?('CD')).to be true
+      expect(string.roman?('CM')).to be true
+      expect(string.roman?('IC')).to be false
+      expect(string.roman?('LC')).to be false
+      expect(string.roman?('VX')).to be false
+      expect(string.roman?('DM')).to be false
+    end
+    it 'should disallow biconsecutive valid order exceptions' do
+      expect(string.roman?('CDM')).to be false
+      expect(string.roman?('IXC')).to be false
+      expect(string.roman?('IVX')).to be false
+      expect(string.roman?('XIXIX')).to be false
+      expect(string.roman?('XVIV')).to be false
+      expect(string.roman?('XIIV')).to be false
+      expect(string.roman?('VIX')).to be false
+      expect(string.roman?('XXC')).to be false
+      expect(string.roman?('CCCXCCC')).to be false
+      expect(string.roman?('LXL')).to be false
+      expect(string.roman?('IXI')).to be false
+      expect(string.roman?('IXXXI')).to be false
+    end
+    it 'should accept valid Roman numerals' do
+      expect(string.roman?('XXX')).to be true
+      expect(string.roman?('XIV')).to be true
+      expect(string.roman?('CXC')).to be true
+      expect(string.roman?('XIX')).to be true
+    end
+  end
+
+  describe 'arabicToRoman' do
+    it 'should return the Roman representation of an Arabic number' do
+      expect(string.arabicToRoman(1666)).to eql('MDCLXVI');
+      expect(string.arabicToRoman(91)).to eql('XCI');
+    end
   end
 end
