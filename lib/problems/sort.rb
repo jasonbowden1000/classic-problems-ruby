aebbd5f724194d63e8f27a73c782020efd58c8df
Jason Bowden <jason.bowden1000@gmail.com>
2018-09-22 12:51:55 -0500

    Find intersection of two sorted arrays

--- lib/problems/sort.rb
+++ lib/problems/sort.rb
@@ -1,5 +1,24 @@
 module Problems
   class Sort
+    def intersect_sorted(a, b)
+      i = 0
+      j = 0
+      intersection = []
+
+      while (i < a.size && j < b.size)
+        if a[i] == b[j] && (i == 0 || a[i] != a[i - 1])
+          intersection << a[i]
+          i += 1
+          j += 1
+        elsif a[i] < b[j]
+          i += 1
+        else
+          j += 1
+        end
+      end
+      intersection
+    end
+
     def merge_sorted(a, b)
       a = a.dup.reverse!
       b = b.dup.reverse!
--- spec/problems/sort_spec.rb
+++ spec/problems/sort_spec.rb
@@ -6,12 +6,21 @@ describe 'Sorter' do
 
   context "when merging sorted arrays" do
     it 'should return all elements in order' do
-      a = [5, 7, 13, 13, 17];
-      b = [3, 7, 11, 19];
-      merged = [3, 5, 7, 7, 11, 13, 13, 17, 19];
+      a = [5, 7, 13, 13, 17]
+      b = [3, 7, 11, 19]
+      merged = [3, 5, 7, 7, 11, 13, 13, 17, 19]
       expect(sorter.merge_sorted(a, b)).to eql(merged)
       expect(sorter.merge_sorted(b, a)).to eql(merged)
       expect(sorter.merge_sorted(a, b)).not_to eql([1, 2, 3, 4, 5, 6, 7])
+    end
+  end
+  context "when finding the intersection of two sorted arrays" do
+    it "should return a sorted array free of duplicates" do
+      a = [2, 2, 3, 3, 5, 5, 6, 7, 7, 8, 12]
+      b = [2, 2, 5, 6, 6, 8, 8, 9, 10]
+      intersection = [2, 5, 6, 8]
+      expect(sorter.intersect_sorted(a, b)).to eql(intersection)
+      expect(sorter.intersect_sorted(b, a)).to eql(intersection)
     end
   end
 end
