9104e01d6575adb84ec89d8aa9c891fd2fa72c47
Jason Bowden <jason.bowden1000@gmail.com>
2018-09-22 19:00:25 -0500

    rubified intersection of sorted arrays

--- lib/problems/sort.rb
+++ lib/problems/sort.rb
@@ -1,22 +1,7 @@
 module Problems
   class Sort
     def intersect_sorted(a, b)
-      i = 0
-      j = 0
-      intersection = []
-
-      while (i < a.size && j < b.size)
-        if a[i] == b[j] && (i == 0 || a[i] != a[i - 1])
-          intersection << a[i]
-          i += 1
-          j += 1
-        elsif a[i] < b[j]
-          i += 1
-        else
-          j += 1
-        end
-      end
-      intersection
+      a & b
     end
 
     def merge_sorted(a, b)
